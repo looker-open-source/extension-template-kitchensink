6d7ed29c3bedd8b547bfe28ca0e2020687a30406
Luke Bowerman <34253496+lukelooker@users.noreply.github.com>
2020-12-11 16:00:24 -0800

    Upgrade to Webpack 5 (#47)
    * Upgrade to Webpack 5
    
    * webpack 5 stricter about accessing environment variables from process.env.
    
    With this change, the webpack EnvironmentPlugin is now used to access those environment variables that are actually required rather than all of the environment variables defined in .env. The added advantage is that the EnvironmentPlugin gives an explicit error message about missing variables when the development server is started.
    
    In addition, as the README did not define the POSTS_SERVER_URL environment variable (which is required), a default value is provided.
    
    * Create .env if it does not exist
    
    Co-authored-by: Bryn Ryans <bryn.ryans@looker.com>

--- webpack.config.js
+++ webpack.config.js
@@ -22,11 +22,25 @@
  * THE SOFTWARE.
  */
 
+const fs = require('fs')
 const path = require('path')
 const webpack = require('webpack')
-const env_config = require('./env_config')
+const dotenv = require('dotenv')
+
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin
+
+// Create .env file if it does not exist
+if (!fs.existsSync('.env')) {
+  fs.copyFileSync('.env_example', '.env')
+}
+dotenv.config()
+if (!process.env.POSTS_SERVER_URL) {
+  // webpack 5 is stricter about environment variables. The POSTS_SERVER_URL
+  // environment variable was not mentioned in the README so default it for
+  // those developers who may have created a .env file without the variable.
+  process.env.POSTS_SERVER_URL = 'http://127.0.0.1:3000'
+}
 
 const PATHS = {
   app: path.join(__dirname, 'src/index.tsx'),
@@ -53,11 +67,18 @@ module.exports = {
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+    fallback: { buffer: false },
   },
   plugins: [
     new BundleAnalyzerPlugin({
       analyzerMode: process.env.ANALYZE_MODE || 'disabled',
     }),
-    new webpack.DefinePlugin(env_config()),
+    new webpack.EnvironmentPlugin([
+      'GOOGLE_CLIENT_ID',
+      'GITHUB_CLIENT_ID',
+      'AUTH0_CLIENT_ID',
+      'AUTH0_BASE_URL',
+      'POSTS_SERVER_URL',
+    ]),
   ],
 }
